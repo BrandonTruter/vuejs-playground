ff1d0f1021aeab31dc7d06353c09c2d43f614aa0
BrandonTruter <brandon.truter@ncino.com>
2024-12-05 08:21:36 +0000

    add front-end implementation

---
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Shopping Cart</h1>
+  <div v-if="cartItems.length > 0">
+    <ShoppingCartList :products="cartItems" />
+    <button class="checkout-button">Proceed to Checkout</button>
+  </div>
+  <div v-if="cartItems.length === 0">
+    You currently have no items in your cart!
+  </div>
+</template>
+
+<script>
+import { cartItems } from '../temp-data';
+import ShoppingCartList from '@/components/ShoppingCartList.vue';
+
+export default {
+  name: "ShoppingCartPage",
+  data() {
+    return {
+      cartItems
+    }
+  },
+  components: {
+    ShoppingCartList
+  }
+}
+</script>
