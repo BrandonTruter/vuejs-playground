6c1a52af7fa44dfbef6f951a7b153729f00c9729
BrandonTruter <brandon.truter@ncino.com>
2024-12-06 14:46:31 +0000

    Build app to deploy first release

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,94 +1,85 @@
-import express from "express";
-import { MongoClient } from "mongodb";
-import path from "path";
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import path from 'path';
 
 async function start() {
-  const databaseName = "myFirstDatabase";
   const databaseURL = process.env.DB_URL;
   const client = new MongoClient(databaseURL);
 
   await client.connect();
-  const db = client.db(databaseName);
+  const db = client.db('vuejs-db');
 
   const app = express();
   app.use(express.json());
-  app.use("/images", express.static(path.join(__dirname, "../assets")));
 
-  app.get("/api/products", async (req, res) => {
-    const products = await db.collection("products").find({}).toArray();
+  app.use('/images', express.static(path.join(__dirname, '../assets')));
+
+  app.use(express.static(
+    path.resolve(__dirname, '../dist'),
+    { maxAge: '1y', etag: false },
+  ));
+
+  app.get('/api/products', async (req, res) => {
+    const products = await db.collection('products').find({}).toArray();
     res.send(products);
   });
 
-  async function populatedCartIds(ids) {
-    const productIds = Array.from(ids).filter((id) => id !== null);
-
-    return Promise.all(
-      productIds.map((id) => db.collection("products").findOne({ id }))
-    );
+  async function populateCartIds(ids) {
+    return Promise.all(ids.map(id => db.collection('products').findOne({ id })));
   }
 
-  app.get("/api/users/:userId/cart", async (req, res) => {
-    const user = await db
-      .collection("users")
-      .findOne({ id: req.params.userId });
-
-    const populatedCart = await populatedCartIds(user.cartItems);
+  app.get('/api/users/:userId/cart', async (req, res) => {
+    const user = await db.collection('users').findOne({ id: req.params.userId });
+    const populatedCart = await populateCartIds(user?.cartItems || []);
     res.json(populatedCart);
   });
 
-  app.get("/api/products/:productId", async (req, res) => {
+  app.get('/api/products/:productId', async (req, res) => {
     const productId = req.params.productId;
-    const product = await db.collection("products").findOne({ id: productId });
+    const product = await db.collection('products').findOne({ id: productId });
     res.json(product);
   });
 
-  // USERS CART ENDPOINTS
-
-  app.post("/api/users", async (req, res) => {
-    const userId = req.body.id;
-
-    await db.collection("users").insertOne({ id: userId });
-
-    const user = await db.collection("users").findOne({ id: userId });
-    res.json(user);
-  });
-
-  app.post("/api/users/:userId/cart", async (req, res) => {
+  app.post('/api/users/:userId/cart', async (req, res) => {
     const userId = req.params.userId;
     const productId = req.body.id;
 
-    await db.collection("users").updateOne(
-      { id: userId },
-      {
-        $addToSet: { cartItems: productId },
-      }
-    );
+    const existingUser = await db.collection('users').findOne({ id: userId });
 
-    const user = await db
-      .collection("users")
-      .findOne({ id: req.params.userId });
-    const populatedCart = await populatedCartIds(user.cartItems);
+    if (!existingUser) {
+      await db.collection('users').insertOne({ id: userId, cartItems: [] });
+    }
+
+    await db.collection('users').updateOne({ id: userId }, {
+      $addToSet: { cartItems: productId }
+    });
+
+    const user = await db.collection('users').findOne({ id: req.params.userId });
+    const populatedCart = await populateCartIds(user?.cartItems || []);
     res.json(populatedCart);
   });
 
-  app.delete("/api/users/:userId/cart/:productId", async (req, res) => {
+  app.delete('/api/users/:userId/cart/:productId', async (req, res) => {
     const userId = req.params.userId;
     const productId = req.params.productId;
 
-    await db.collection("users").updateOne(
-      { id: userId },
-      {
-        $pull: { cartItems: productId },
-      }
-    );
+    await db.collection('users').updateOne({ id: userId }, {
+      $pull: { cartItems: productId },
+    });
 
-    const user = await db.collection("users").findOne({ id: userId });
-    const populatedCart = await populatedCartIds(user.cartItems);
+    const user = await db.collection('users').findOne({ id: req.params.userId });
+    const populatedCart = await populateCartIds(user?.cartItems || []);
     res.json(populatedCart);
   });
 
-  app.listen(8000, () => {
-    console.log("Server is listening on port 8000");
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+  });
+
+  const port = process.env.PORT || 8000;
+
+  app.listen(port, () => {
+    console.log('Server is listening on port ' + port)
   });
 }
 
--- front-end/src/components/NavBar.vue
+++ front-end/src/components/NavBar.vue
@@ -5,21 +5,27 @@
         <img :src="logo" />
       </div>
     </router-link>
-    <router-link to="/cart" class="cart-link">
-      <button>Shopping Cart</button>
-    </router-link>
+    <div class="nav-buttons-wrap">
+      <button @click="signOut" v-if="user">Sign Out</button>
+      <router-link to="/cart">
+        <button>Shopping Cart</button>
+      </router-link>
+    </div>
+
   </div>
 </template>
 
 <script>
-  import logo from '@/assets/logo-hexagon.svg'
+import { getAuth, signOut } from 'firebase/auth';
 
-  export default {
-    name: "NavBar",
-    data() {
-      return {
-        logo,
-      }
+export default {
+  name: "NavBar",
+  props: ['user'],
+  methods: {
+    signOut() {
+      const auth = getAuth();
+      signOut(auth);
     }
   }
+}
 </script>
